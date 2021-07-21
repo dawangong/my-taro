c4d0d69f575a57988805565a3b93bca1c12cf9ea
wh.q <dawangong@126.com>
2021-07-21 22:17:17 +0800

    add taro ui and request

--- src/utils/test.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-07-21 18:55:43
- * @LastEditTime: 2021-07-21 18:55:43
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /my-taro/src/utils/test.ts
- */
