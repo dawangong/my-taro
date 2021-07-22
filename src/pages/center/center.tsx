b51aabc0e846e531c8a6c9a19f3c9cada50028b3
wh.q <dawangong@126.com>
2021-07-22 15:47:07 +0800

    add center

--- src/pages/center/center.tsx
+++ src/pages/center/center.tsx
@@ -2,7 +2,7 @@ import './center.scss'
 
 import React, { useEffect, useContext } from 'react'
 import { View, Button, Text } from '@tarojs/components'
-import {
+import Taro, {
   useReady,
   useDidShow,
   useDidHide,
@@ -10,6 +10,7 @@ import {
 } from '@tarojs/taro'
 import { observer } from 'mobx-react'
 import counterStore from '../../store/counter'
+import { AtDivider, AtAvatar, AtIcon, AtGrid } from 'taro-ui'
 
 
 interface Props {}
@@ -34,10 +35,40 @@ const Center: React.FC<Props> = (props: Props) => {
 
   return (
     <View className='page-center'>
-      <Button onClick={increment}>+</Button>
-      <Button onClick={decrement}>-</Button>
-      <Button onClick={incrementAsync}>Add Async</Button>
-      <Text>{counter}</Text>
+      <View className='page-center__info' onClick={() => Taro.navigateTo({
+        url: '/pages/profile/profile'
+      })
+    }>
+        <AtAvatar className='page-center__info-avatar' circle image='http://storage.360buyimg.com/mtd/home/32443566_635798770100444_2113947400891531264_n1533825816008.jpg'></AtAvatar>
+        <Text>测试商户</Text>
+        <AtIcon value='chevron-right' size='30' color='#c7c7c7' ></AtIcon>
+      </View>
+      <AtDivider height="1" lineColor="#f5f5f5" />
+      <View className='page-center__base'>
+      <AtGrid hasBorder={false} columnNum={4} data={
+        [
+            {
+              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+              value: '卡券列表'
+            },
+            {
+              image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+              value: '核销卡券'
+            }
+          ]
+        } />
+      </View>
+      <View className='page-center__service'>
+        <View className='page-center__service__title'>常用服务</View>
+        <View className='page-center__service-content'>
+          <View className='page-center__service-item page-center__service-item--active'>
+            <Text className='page-center__service__desc'>营销活动</Text>
+          </View>
+          <View className='page-center__service-item page-center__service-item--video'>
+            <Text className='page-center__service__desc'>视频素材</Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 };
