2cf90530d9c1e734d580107aeb07e19350124126
wh.q <dawangong@126.com>
2021-07-27 17:36:45 +0800

    add basic ui design for split detail

--- src/pages/split-detail/split-detail.tsx
+++ src/pages/split-detail/split-detail.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-26 17:05:54
+ * @LastEditTime: 2021-07-27 17:35:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my-taro/src/pages/split-detail/split-detail.tsx
+ */
 import './split-detail.scss'
 
 import React, { useEffect, useState, useContext } from 'react'
@@ -9,7 +17,7 @@ import Taro, {
 } from '@tarojs/taro'
 import { observer } from 'mobx-react'
 import { View, Text } from '@tarojs/components'
-import { AtInput, AtButton } from 'taro-ui'
+import { AtInput, AtButton, AtCard } from 'taro-ui'
 // import counterStore from '../../store/counter'
 
 
@@ -34,7 +42,20 @@ const SplitDetail: React.FC<Props> = (props: Props) => {
 
   return (
     <View className='page-split-detail'>
-      content
+      <View className='page-coupon-edit__bg'></View>
+      <View className='page-coupon-edit__content'>
+        <AtCard
+          className='page-coupon-edit__card'
+          title='基本信息'
+        >
+        </AtCard>
+        <AtCard
+          className='page-coupon-edit__card'
+          title='扫码详情'
+        >
+        </AtCard>
+        <AtButton type='primary' className='page-coupon-edit__btn' onClick={() => {}}>下载二维码</AtButton>
+      </View>
     </View>
   )
 };
